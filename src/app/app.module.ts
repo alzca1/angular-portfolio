87019f4f06f5477cd0e55e305442d524f9bbd0bc
Álvaro Zájara <alzca1@gmail.com>
2020-03-06 10:24:55 +0100

    created admin components and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import {HeaderComponent} from "./header/header.component";
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import {AdminModule} from './admin/admin.module';
 
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, AboutComponent, ContactComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, AdminModule],
   providers: [],
   bootstrap: [AppComponent]
 })
